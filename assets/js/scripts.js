c28e621516f4667737e635c81d1cd6a006655269
Ibnu Fauzy <58937373+ibnfzy@users.noreply.github.com>
2024-02-13 09:35:36 +0800

    day-4 done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -67,7 +67,7 @@ function renderProject() {
             <div class="project-card">
               <div class="card-body-project">
                 <img src="./assets/img/stock-img.jpg" alt="" class="card-img-project" />
-                <h3 class="card-title">${project.name}</h3>
+                <h3 class="card-title"><a href='./detail_projects.html' target='_blank'>${project.name}</a></h3>
                 <p class="card-date">durasi : 3 bulan</p>
                 <p>
                   ${project.desc}
