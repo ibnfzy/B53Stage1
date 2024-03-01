5c8bb480304196bf72b8e0c9d62d68d54373e58c
Ibnu Fauzy <58937373+ibnfzy@users.noreply.github.com>
2024-03-01 16:24:07 +0800

    day 10 add partials to hbs

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 import express from "express";
+import hbs from "hbs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Config Express JS
 const app = express();
 const port = 3000;
 
+// Get Directory Root
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Config hbs
 app.set("view engine", "hbs");
 app.set("views", "src/views");
-
 app.use("/assets", express.static("src/assets"));
+hbs.registerPartials(__dirname + "/src/views/partials", (err) => {
+  return console.log(err);
+});
+hbs.registerHelper("if_equal", (value1, value2, opts) => {
+  if (value1 === value2) {
+    return opts.fn(this);
+  } else {
+    return opts.inverse(this);
+  }
+});
 
+// Routes
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -27,6 +47,7 @@ app.get("/testimonials", (req, res) => {
   res.render("testimonials");
 });
 
+// Express JS listener
 app.listen(port, () => {
   console.log(`Express Server run at : http://localhost:${port}`);
 });
